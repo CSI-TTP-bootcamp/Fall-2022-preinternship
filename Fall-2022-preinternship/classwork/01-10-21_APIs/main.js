bdc46ec3a92631201346978d83df963687efb050
CSI-TTP-bootcamp <76490185+CSI-TTP-bootcamp@users.noreply.github.com>
2021-11-02 16:20:47 -0400

    Delete Fall-2022-preinternship/classwork/01-10-21_APIs directory

--- Fall-2022-preinternship/classwork/01-10-21_APIs/main.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios").default;
-axios
-  .get("https://jsonplaceholder.typicode.com/todos")
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
